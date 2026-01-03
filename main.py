2bd0091e9a9dda6c72b8001e5a6c853b1e61ba2b
harsh-pratapsingh24 <harsh.psingh2005@gmail.com>
2026-01-03 14:31:28 +0530

    updated the graphs and data extraction through yfinance

--- main.py
+++ main.py
@@ -18,7 +18,32 @@ selected_stocks = st.selectbox("Select dataset for prediction",stocks)
 n_years = st.slider("Years of prediction:",1 , 4)
 period = n_years * 365
 
+@st.cache_data
 def load_data(ticker):
-    data = yf.download(ticker,START,TODAY)
+    data = yf.download(ticker, START, TODAY, multi_level_index=False)
+    if data is None or data.empty:
+        st.error(f"No data found for {ticker}")
+        return None
     data.reset_index(inplace=True)
     return data
+
+data_load_state = st.text("LOAD DATA...")
+data = load_data(selected_stocks)
+data_load_state.text("LOADING DATA...DONE")
+
+st.subheader("RAW DATA")
+if data is None:
+    st.info("No data to display for the selected ticker.")
+else:
+    st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y =data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y =data['Close'], name='stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+# FORECASTING 
